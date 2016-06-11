08ddcb70511ebd2a38a75761070e56c0681f6d04
Kelauni22 <Kelauni.Cook@gmail.com>
2016-06-11 18:42:49 -0400

    Asked user for coordinates: Wrote loop to print new grid with hit shown

--- Battleship/Program.cs
+++ Battleship/Program.cs
@@ -49,41 +49,70 @@ namespace Battleship
             }
 
             //Make coordinates that equal false
+            
+
+           
+            
+
+                base_grid[0, 4] = false;
+                base_grid[0, 9] = false;
+                base_grid[2, 5] = false;
+                base_grid[3, 1] = false;
+                base_grid[4, 4] = false;
+                base_grid[8, 8] = false;
+                base_grid[7, 6] = false;
+                base_grid[2, 2] = false;
+                base_grid[5, 9] = false;
+                base_grid[9, 7] = false;
 
 
-            base_grid[0,4] = false;
-            base_grid[0,9] = false;
-            base_grid[2,5] = false;
-            base_grid[3,1] = false;
-            base_grid[4,4] = false;
-            base_grid[8,8] = false;
-            base_grid[7,6] = false;
-            base_grid[10,2] = false;
-            base_grid[10,9] = false;
-            base_grid[9,7] = false;
+                //Write an if statement if person hits a spot, display new screen
 
-            for (int x = 0; x < 10; x++)
+                Console.WriteLine("\n" + "Pick a number for coordinate 'x'");
+                string x_pick_string = Console.ReadLine();
+                int x_pick = Convert.ToInt16(x_pick_string);
+
+
+                Console.WriteLine("Pick a number for coordinate 'y'");
+                string y_pick_string = Console.ReadLine();
+                int y_pick = Convert.ToInt16(y_pick_string);
+
+            if (base_grid[x_pick, y_pick] == false)
             {
-                for (int y = 0; y < 10; y++)
+                Console.Clear();
+                bool[,] new_grid = new bool[10, 10];
+
+                for (int x = 0; x < 10; x++)
+
                 {
-                    if (base_grid[x,y] == false)
+                    for (int y = 0; y < 10; y++)
                     {
-                        Console.WriteLine("*");
+                        Console.Write("0");
+                        if (new_grid[x, y] == base_grid[x_pick, y_pick])
+                        {
+                            Console.Write("*");
+                        }
+
+                        Console.Write("\n");
                     }
+
                 }
+
             }
 
+            else
+            {
+                Console.Write("\n" + "Sorry, you missed!");
+            }
 
+                Console.ReadLine();
 
-            Console.ReadLine();
+            }
 
-
-                        
-            //Write an if statement if person hits a spot, display new screen
             //Write an if statement if person misses, keep same screen
             //Keep looping until all spots have been hit
             //Say good game once false
 
-        }
+        
     }
 }
