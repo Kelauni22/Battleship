c2c1a6c1240d08a929b64504889eb515d6cd335d
Kelauni22 <Kelauni.Cook@gmail.com>
2016-06-11 17:24:03 -0400

    Declared which points are the ships

--- Battleship/Program.cs
+++ Battleship/Program.cs
@@ -48,33 +48,37 @@ namespace Battleship
 
             }
 
-            
+            //Make coordinates that equal false
+
+
+            base_grid[0,4] = false;
+            base_grid[0,9] = false;
+            base_grid[2,5] = false;
+            base_grid[3,1] = false;
+            base_grid[4,4] = false;
+            base_grid[8,8] = false;
+            base_grid[7,6] = false;
+            base_grid[10,2] = false;
+            base_grid[10,9] = false;
+            base_grid[9,7] = false;
+
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    if (base_grid[x,y] == false)
+                    {
+                        Console.WriteLine("*");
+                    }
+                }
+            }
 
 
 
             Console.ReadLine();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            //Make coordinates that equal false
+                        
             //Write an if statement if person hits a spot, display new screen
             //Write an if statement if person misses, keep same screen
             //Keep looping until all spots have been hit
